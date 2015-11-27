d4adf99ad72077a989bf95f7f6f31f1533620d39
Mike Houston <mike@kothar.net>
2015-11-27 19:10:32 +0000

    Allow simultaneous get of several files

--- b2/get.go
+++ b2/get.go
@@ -7,6 +7,8 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"path/filepath"
+	"sync"
 
 	"github.com/gosuri/uiprogress"
 	"github.com/gosuri/uiprogress/util/strutil"
@@ -18,6 +20,7 @@ import (
 // TODO support destination path
 // TODO support version id downloads
 type Get struct {
+	Threads int `short:"j" long:"threads" default:"5" description:"Maximum simultaneous downloads to process"`
 }
 
 func init() {
@@ -41,16 +44,46 @@ func (o *Get) Execute(args []string) error {
 	}
 
 	uiprogress.Start()
+	fmt.Printf("Making a pool for %d downloads\n", o.Threads)
+	pool := make(chan bool, o.Threads)
+	group := sync.WaitGroup{}
+	var downloadError error
+
 	for _, file := range args {
 		// TODO handle wildcards
-		err := download(bucket, file)
+
+		fileInfo, reader, err := bucket.DownloadFileByName(file)
 		if err != nil {
-			return err
+			downloadError = err
+			break
 		}
 
+		// Get a ticket to process a download
+		pool <- true
+
+		if downloadError != nil {
+			break
+		}
+
+		group.Add(1)
+		go func() {
+			defer func() {
+				// Allow next entry into pool
+				group.Done()
+				<-pool
+			}()
+
+			err := download(fileInfo, reader, file)
+			if err != nil {
+				fmt.Println(err)
+				downloadError = err
+			}
+		}()
 	}
 
-	return nil
+	group.Wait()
+
+	return downloadError
 }
 
 type progressWriter struct {
@@ -64,20 +97,26 @@ func (p *progressWriter) Write(b []byte) (int, error) {
 	return written, err
 }
 
-func download(bucket *backblaze.Bucket, file string) error {
-	fileInfo, reader, err := bucket.DownloadFileByName(file)
-	if err != nil {
-		return err
-	}
+func download(fileInfo *backblaze.File, reader io.ReadCloser, path string) error {
 	defer reader.Close()
 
+	// Display download progress
 	bar := uiprogress.AddBar(int(fileInfo.ContentLength))
+	bar.AppendFunc(func(b *uiprogress.Bar) string {
+		speed := (float32(b.Current()) / 1024) / float32(b.TimeElapsed().Seconds())
+		return fmt.Sprintf("%7.2f KB/s", speed)
+	})
 	bar.AppendCompleted()
 	bar.PrependFunc(func(b *uiprogress.Bar) string { return fmt.Sprintf("%10d", b.Total) })
 	bar.PrependFunc(func(b *uiprogress.Bar) string { return strutil.Resize(fileInfo.Name, 50) })
-	bar.Width = 30
+	bar.Width = 20
 
-	writer, err := os.Create(file)
+	err := os.MkdirAll(filepath.Dir(path), 0777)
+	if err != nil {
+		return err
+	}
+
+	writer, err := os.Create(path)
 	if err != nil {
 		return err
 	}
@@ -91,7 +130,7 @@ func download(bucket *backblaze.Bucket, file string) error {
 		return err
 	}
 
-	// Check sha
+	// Check SHA
 	sha1Hash := hex.EncodeToString(sha.Sum(nil))
 	if sha1Hash != fileInfo.ContentSha1 {
 		return errors.New("Downloaded data does not match SHA1 hash")
