373819725fc560fa962c6cd883b533d2ebec4844
Mike Houston <mike@kothar.net>
2015-12-13 18:31:43 +0000

    Fix download output path + Add --discard option

--- b2/get.go
+++ b2/get.go
@@ -6,6 +6,7 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"os"
 	"path/filepath"
 	"sync"
@@ -19,13 +20,13 @@ import (
 )
 
 // TODO support subdirectories
-// TODO support destination path
 // TODO support version id downloads
 
 // Get is a command
 type Get struct {
 	Threads int    `short:"j" long:"threads" default:"5" description:"Maximum simultaneous downloads to process"`
 	Output  string `short:"o" long:"output" default:"." description:"Output file name or directory"`
+	Discard bool   `long:"discard" description:"Discard downloaded data"`
 }
 
 func init() {
@@ -62,7 +63,9 @@ func (o *Get) Execute(args []string) error {
 		if info.IsDir() {
 			outDir = o.Output
 		} else if len(args) > 1 {
-			return errors.New("Single output file specified for multiple targets: " + o.Output)
+			return errors.New("Single (existing) output file specified for multiple targets: " + o.Output)
+		} else {
+			outName = o.Output
 		}
 	} else if os.IsNotExist(err) {
 		parent := filepath.Dir(o.Output)
@@ -71,9 +74,10 @@ func (o *Get) Execute(args []string) error {
 			return errors.New("Directory does not exist: " + parent)
 		}
 		if len(args) > 1 {
-			return errors.New("Single output file specified for multiple targets: " + o.Output)
+			outDir = o.Output
+		} else {
+			outName = o.Output
 		}
-		outName = o.Output
 	} else {
 		return err
 	}
@@ -102,7 +106,7 @@ func (o *Get) Execute(args []string) error {
 		}
 		path := filepath.Join(outDir, name)
 		go func(fileInfo *backblaze.File, reader io.ReadCloser, path string) {
-			err := download(fileInfo, reader, path)
+			err := download(fileInfo, reader, path, o)
 			if err != nil {
 				fmt.Println(err)
 				downloadError = err
@@ -130,21 +134,25 @@ func (p *progressWriter) Write(b []byte) (int, error) {
 	return written, err
 }
 
-func download(fileInfo *backblaze.File, reader io.ReadCloser, path string) error {
+func download(fileInfo *backblaze.File, reader io.ReadCloser, path string, o *Get) error {
 	defer reader.Close()
 
-	err := os.MkdirAll(filepath.Dir(path), 0777)
-	if err != nil {
-		return err
+	var writer = ioutil.Discard
+	if !o.Discard {
+		fmt.Printf("Downloading to: %s\n", path)
+		err := os.MkdirAll(filepath.Dir(path), 0777)
+		if err != nil {
+			return err
+		}
+
+		file, err := os.Create(path)
+		if err != nil {
+			return err
+		}
+		defer file.Close()
+		writer = file
 	}
 
-	writer, err := os.Create(path)
-	if err != nil {
-		return err
-	}
-	defer writer.Close()
-
-	var w io.Writer = writer
 	if opts.Verbose {
 		bar := uiprogress.AddBar(int(fileInfo.ContentLength))
 		start := time.Now()
@@ -162,13 +170,13 @@ func download(fileInfo *backblaze.File, reader io.ReadCloser, path string) error
 		bar.PrependFunc(func(b *uiprogress.Bar) string { return strutil.Resize(fileInfo.Name, 50) })
 		bar.Width = 20
 
-		w = &progressWriter{bar, writer}
+		writer = &progressWriter{bar, writer}
 	}
 
 	sha := sha1.New()
-	tee := io.MultiWriter(sha, w)
+	tee := io.MultiWriter(sha, writer)
 
-	_, err = io.Copy(tee, reader)
+	_, err := io.Copy(tee, reader)
 	if err != nil {
 		return err
 	}
